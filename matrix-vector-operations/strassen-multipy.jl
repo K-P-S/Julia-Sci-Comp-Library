4a4d3d6b62c23ac622a4601cf2352207acb0f96b
K <keegansabo@gmail.com>
2017-04-15 21:10:58 -0400

    Update strassen-multipy.jl

--- matrix-vector-operations/strassen-multipy.jl
+++ matrix-vector-operations/strassen-multipy.jl
@@ -2,5 +2,20 @@
 #accepts square matrices A,B (assumes nxn, mxm where n,m are powers of two)
 #returns matrix C
 
+function partition(A)
+  #add try-catch
+  #accepts square matrix A
+  n = length(A)[1]
+  for 
 
+function multiply(A,B)
+  n = length(A)[1]
+  #new matrix C
+  C = new zeroesMatrix(length(A)[1],length(B)[2])
+  if n == 1
+    C[1:1] = A[1:1] * B[1:1]
+  else
+    
+  
+ 
 
